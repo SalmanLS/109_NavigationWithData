b9847c5913cfd142170bd3f6b09b238f273a06e9
SalmanLS <salmanluthfi456@gmail.com>
2023-11-13 19:04:36 +0700

    menambah column dan values string

--- app/src/main/java/com/example/navdata/HalamanForm.kt
+++ app/src/main/java/com/example/navdata/HalamanForm.kt
@@ -1,10 +1,18 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.navdata
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun HalamanForm(
@@ -15,5 +23,13 @@ fun HalamanForm(
     var tlpnTxt by remember { mutableStateOf("")}
 
     var listData: MutableList<String> = mutableListOf(namaTxt,alamatTxt,tlpnTxt)
+
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ){
+
+    }
 
 }
