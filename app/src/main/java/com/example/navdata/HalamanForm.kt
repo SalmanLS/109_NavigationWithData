f655e1ad45d121333d4f32dd89425393e612748d
SalmanLS <salmanluthfi456@gmail.com>
2023-11-13 19:06:40 +0700

    menambah 3 buah form pada halaman form

--- app/src/main/java/com/example/navdata/HalamanForm.kt
+++ app/src/main/java/com/example/navdata/HalamanForm.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,23 +15,47 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun HalamanForm(
     onSubmitButtonClicked: (MutableList<String>) -> Unit
 ) {
-    var namaTxt by remember{ mutableStateOf("")}
-    var alamatTxt by remember{ mutableStateOf("")}
-    var tlpnTxt by remember { mutableStateOf("")}
+    var namaTxt by remember { mutableStateOf("") }
+    var alamatTxt by remember { mutableStateOf("") }
+    var tlpnTxt by remember { mutableStateOf("") }
 
-    var listData: MutableList<String> = mutableListOf(namaTxt,alamatTxt,tlpnTxt)
+    var listData: MutableList<String> = mutableListOf(namaTxt, alamatTxt, tlpnTxt)
 
-    Column (
+    Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxSize()
-    ){
-
+    ) {
+        OutlinedTextField(value = namaTxt, onValueChange = { namaTxt = it }, label = {
+            Text(
+                text = stringResource(
+                    id = R.string.namap
+                )
+            )
+        }
+        )
+        OutlinedTextField(value = alamatTxt, onValueChange = { alamatTxt = it }, label = {
+            Text(
+                text = stringResource(
+                    id = R.string.alamatp
+                )
+            )
+        }
+        )
+        OutlinedTextField(value = tlpnTxt, onValueChange = { tlpnTxt = it }, label = {
+            Text(
+                text = stringResource(
+                    id = R.string.nomorp
+                )
+            )
+        }
+        )
     }
 
 }
