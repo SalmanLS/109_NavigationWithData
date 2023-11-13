9f4d2465c6f19aaea2a3065904b8fb0991ffd97a
SalmanLS <salmanluthfi456@gmail.com>
2023-11-13 18:47:04 +0700

    menambah data class nama,alamat, dan tlp

--- app/src/main/java/com/example/navdata/data/OrderUIState.kt
+++ app/src/main/java/com/example/navdata/data/OrderUIState.kt
@@ -3,5 +3,8 @@ package com.example.navdata.data
 data class OrderUiState(
     val jumlah: Int = 0,
     val rasa: String ="",
-    val harga: String = ""
+    val harga: String = "",
+    val nama: String ="",
+    val alamat: String = "",
+    val tlp: String = ""
 )
