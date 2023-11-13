02bff06954162e036921a78d0b44e8eb1e874567
SalmanLS <salmanluthfi456@gmail.com>
2023-11-13 19:08:42 +0700

    menambah spacer dan button pada halaman form

--- app/src/main/java/com/example/navdata/HalamanForm.kt
+++ app/src/main/java/com/example/navdata/HalamanForm.kt
@@ -4,7 +4,10 @@ package com.example.navdata
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -16,6 +19,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HalamanForm(
@@ -56,6 +60,11 @@ fun HalamanForm(
             )
         }
         )
+
+        Spacer(modifier = Modifier.height(15.dp))
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(id = R.string.confirm))
+        }
     }
 
 }
